f82af8fe7460ec4ebba8333080221121253f9747
Jonathan J Wagner <frostmute@gmail.com>
2026-06-24 23:54:41 -0500

    fix(tests): upgrade testing dependencies to fix jest-environment-jsdom errors

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('jest').Config} */
 module.exports = {
 	preset: "ts-jest",
-	testEnvironment: "node",
-	roots: ["<rootDir>/src"],
+	testEnvironment: "jsdom",
+	roots: ["<rootDir>"],
 	testMatch: ["**/__tests__/**/*.test.ts"],
 	collectCoverageFrom: ["src/**/*.ts", "!src/**/__tests__/**"],
 	moduleNameMapper: {
 		"^obsidian$": "<rootDir>/src/__mocks__/obsidian.ts",
 	},
+	transform: {
+		"^.+\\\\.tsx?$": [
+			"ts-jest",
+			{
+				tsconfig: "tsconfig.json",
+				useESM: true
+			}
+		]
+	}
 };
